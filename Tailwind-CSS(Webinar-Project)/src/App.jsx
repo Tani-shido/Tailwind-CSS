e375690845ac57d504ea41162568165a962d510d
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-09-13 19:42:15 +0530

    Update App.jsx

--- Tailwind-CSS(Webinar-Project)/src/App.jsx
+++ Tailwind-CSS(Webinar-Project)/src/App.jsx
@@ -1,16 +1,24 @@
+import React from "react"
 import './App.css'
-import { SideBar } from './components/User-Page/side-bar'
-import { MainSection } from './components/User-Page/Main-Section'
+import { PageOne } from "./Page-1"
+import { PageTwo } from "./Page-2"
+import { PageThree } from "./Page-3"
+import { SideBar } from "./Home-Page/Side-Bar"
+import { MainContent } from "./Home-Page/Main-Content"
+
 function App() {
 
   return <>
-  <div className='max-w-[1920px] max-h-screen mx-auto'>
-    <div className='h-screen w-full grid grid-cols-5'>
-      <div className='border-r-2 border-gray-300 col-span-1 h-full w-full hidden 2xl:block' ><SideBar /></div>
-      <div className='border-none bg-cover col-span-4 h-full w-full ' ><MainSection /></div>
+    <div className="bg-blue-300 w-screen h-screen text-lg">
+      <PageOne />
+      <PageTwo />
+      <PageThree /> 
+      <div className="w-full h-full flex">
+        <SideBar />
+        <MainContent />
+      </div>
     </div>
-  </div>
- </>
+  </>
 }
 
 export default App
