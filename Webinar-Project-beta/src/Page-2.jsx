6db063072b9f02b78dad1b142d335169ba0b1545
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-09-15 20:01:04 +0530

    Update Page-2.jsx

--- Webinar-Project-beta/src/Page-2.jsx
+++ Webinar-Project-beta/src/Page-2.jsx
@@ -3,12 +3,13 @@ import { Button } from "./Components/Buttons"
 
 export function PageTwo(){
     return <>
-    <div className="min-w-screen min-h-screen bg-blue-950 text-white text-center flex flex-col align-center">
+    <div className="min-w-screen min-h-screen pt-40 bg-blue-950 text-white text-center flex flex-col align-center">
       <div id="heroSec" className="text-3xl font-medium">
         <div className="p-16"><span className="text-emerald-400">Webinar</span><span>.gg</span></div>
         <div className="pb-16">Let's get started</div>
       </div>
       <div className="flex flex-col items-center">
+        <p className="text-md pb-4 text-gray-400">Enter your email id</p>
         <Input type={"text"} placeholder={"Email Id"}></Input>
         <Button disabled={false}>Sign-Up</Button>
       </div>
