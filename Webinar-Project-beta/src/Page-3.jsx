c3b3647342ee808034a8ebee114be2e4b73d9da3
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-09-15 20:00:49 +0530

    Update Page-3.jsx

--- Webinar-Project-beta/src/Page-3.jsx
+++ Webinar-Project-beta/src/Page-3.jsx
@@ -1,18 +1,27 @@
 import { Button } from "./Components/Buttons"
-import { OtpInput } from "./Components/OtpInput"
+import { OtpInput } from './Components/OtpInput';
 import { Timer } from "./Components/Timer"
+import { useState } from "react";
 
 
 export function PageThree(){
+    const [otpArray, setOtpArray] = useState([]);
+
     return <>
-    <div className="min-w-screen min-h-screen bg-blue-950 text-white text-center flex flex-col align-center">
+    <div className="min-w-screen min-h-screen pt-40 bg-blue-950 text-white text-center flex flex-col align-center">
        <div id="heroSec" className="text-3xl font-medium">
         <div className="p-16"><span className="text-emerald-400">Webinar</span><span>.gg</span></div>
         <div className="pb-16">Check Your Email For A Code</div>
       </div>
       <div className="flex flex-col items-center">
         <p className="text-md pb-4 text-gray-400">Please enter the verification code sent to your email id _mail-id_</p>
-        <OtpInput type={"text"}></OtpInput>
+         <OtpInput
+          length={6}
+          onComplete={(otpValue) => {
+            console.log('Complete OTP:', otpValue);
+            setOtpArray(otpValue.split(''));
+          }}
+        />
         <Timer />
         <Button disabled={false}>Verify</Button>
         <p className="text-md p-2 text-gray-400">Can't find the code?Click _here_ to resend.</p>
