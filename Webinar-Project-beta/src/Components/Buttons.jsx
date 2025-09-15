55d335057a59f8b2414a06d1e6319e956d5f7cca
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-09-15 20:01:51 +0530

    Update Buttons.jsx

--- Webinar-Project-beta/src/Components/Buttons.jsx
+++ Webinar-Project-beta/src/Components/Buttons.jsx
@@ -3,7 +3,7 @@ export const Button = ({
     children,
     onClick
 }) => {
-    return <div onClick={onClick} className={`cursor-pointer w-xs mt-12 text-md font-medium p-4 outline-none rounded-xl ${disabled ? "bg-gray-400" : "bg-emerald-400"}`}>
+    return <div onClick={(e) => {e.currentTarget.parentElement.parentElement.style.display = 'none';}}className={`cursor-pointer w-xs mt-12 text-md font-medium p-4 outline-none rounded-xl ${disabled ? "bg-gray-400" : "bg-emerald-400"}`}>
         {children}
     </div>
 }
